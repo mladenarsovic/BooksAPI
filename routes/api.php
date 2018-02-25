fd2d5900ddc5029b82449a45a26025009d244760
Mladen <djoml8bre@gmail.com>
2018-02-25 19:04:51 +0100

    Ispravio metode za search rute

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 // Guest routes (without autorization)
 Route::get('books', 'BookController@index');
-Route::post('search-title/{title}', 'BookController@searchByTitle'); // Search by title endpoint
-Route::post('search-year/{year}', 'BookController@searchByYear'); // Search by year endpoint
+Route::get('search-title/{title}', 'BookController@searchByTitle'); // Search by title endpoint
+Route::get('search-year/{year}', 'BookController@searchByYear'); // Search by year endpoint
 // Registration
 Route::post('register', 'UsersController@store');
 Route::post('login', 'Auth\LoginController@authenticate');
 // Authorized actions
-Route::middleware('jwt.auth')->post('book', 'BookController@store');
-Route::middleware('jwt.auth')->delete('book/{id}', 'BookController@destroy');
-Route::middleware('jwt.auth')->put('book/{id}/edit', 'BookController@update');
+Route::middleware('jwt.auth')->post('book', 'BookController@store'); // Create new book
+Route::middleware('jwt.auth')->delete('book/{id}', 'BookController@destroy'); // Delete a book
+Route::middleware('jwt.auth')->put('book/{id}/edit', 'BookController@update'); // Edit a book
